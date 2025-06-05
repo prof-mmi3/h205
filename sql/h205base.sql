c0725efa8df6e61ce7e5078bff8bde98cf9f1088
prof-mmi3 <126324596+prof-mmi3@users.noreply.github.com>
2025-06-05 09:01:52 +0200

    Update h205base.sql

--- sql/h205base.sql
+++ sql/h205base.sql
@@ -40,7 +40,7 @@ LOCK TABLES `abonnes` WRITE;
 INSERT INTO `abonnes` VALUES
 (1,'Patrice','Gommery','mmi24x01'),
 (2,'Florent','Libbrecht','mmi24x99'),
-(3,'Kyllian','Bresson','mmi24z01');
+(3,'Amine','Haraoubia','mmi24z02');
 /*!40000 ALTER TABLE `abonnes` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
